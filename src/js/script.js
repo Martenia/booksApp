561c4241f379104fc499d34c0fb569155a5f18f8
Marta Nitek <marta.nitek@onet.pl>
2021-04-13 14:40:14 +0200

    git commit -m

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,9 @@
 const templates = {
   booksTemplate: Handlebars.compile(document.querySelector('#template-book').innerHTML),
 };
-console.log(templates.booksTemplate);
 
 const booksContainer = document.querySelector('.books-list');
-console.log(booksContainer);
-
-
+// console.log(booksContainer);
 
 function render() {
   for (let items in dataSource.books) {
@@ -16,11 +13,42 @@ function render() {
     console.log(book);
 
     const generatedHTML = templates.booksTemplate(book);
-    console.log(generatedHTML);
+    // console.log(generatedHTML);
     
     booksContainer.appendChild(utils.createDOMFromHTML(generatedHTML));
 
-    console.log(items);
   }
 }
 render();
+
+// Zacznij od dodania nowej pustej tablicy favoriteBooks.
+const favoriteBooks = [];
+
+// Dodaj funkcję initActions.
+function initActions() {
+  // Przygotuj w niej referencję do listy wszystkich elementów .book__image w liście .booksList.
+  const bookImage = document.querySelectorAll('ul.books-list .book__image');
+  console.log(bookImage);
+  
+  // Następnie przejdź po każdym elemencie z tej listy.
+  for (let cover of bookImage){
+    // Dla każdego z nich dodaj nasłuchiwacz, który po wykryciu uruchomi funkcję, która...
+    cover.addEventListener('dblclick', function(event) {
+      // ...zatrzyma domyślne zachowanie przeglądarki (preventDefault),
+      event.preventDefault();
+      // doda do klikniętego elementu klasę favorite,
+      cover.classList.add('favorite');
+      // pobierze z jego data-id identyfikator książki,
+      const bookId = cover.getAttribute('data-id');
+      // i doda ten identyfikator do favoriteBooks
+      favoriteBooks.push(bookId);
+    });
+   
+    
+  }
+}
+initActions();
+
+console.log(favoriteBooks);
+
+
